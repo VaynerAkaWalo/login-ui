c4ecaa9087c1baf29fda05c562eaf1868c7c12ec
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-10-27 23:04:53 +0100

    Use dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,33 @@ import LoginForm from "@components/login-form.tsx";
 import ProfileView from "@components/profile-view.tsx";
 import {Footer} from "@components/footer/Footer.tsx";
 import RegisterForm from "@components/register-form.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FFFFFF'
+    }
+  },
+});
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<Container/>}>
-          <Route index element={<ProfileView/>}/>
-          <Route path="/login" element={<LoginForm/>}/>
-          <Route path="/register" element={<RegisterForm/>}/>
-        </Route>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <div className="bg-zinc-800 text-zinc-200">
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/" element={<Container/>}>
+              <Route index element={<ProfileView/>}/>
+              <Route path="/login" element={<LoginForm/>}/>
+              <Route path="/register" element={<RegisterForm/>}/>
+            </Route>
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   )
 }
 
